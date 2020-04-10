cc500d0e035a7d216cb954495b384aab53a99ecb
Brian Osullivan <brian.osullivan@8x8.com>
2020-04-10 18:49:36 +0100

    test: first unittests

---
+++ tst/test_covid_country.py
@@ -0,0 +1,31 @@
+import unittest
+from src.covid import CovidCountry
+
+
+class CovidCountryTest(unittest.TestCase):
+    """
+    From project root:
+
+        $ python -m unittest discover
+
+    And that will run these tests.
+    """
+    def setUp(self):
+        # Load and fix UK data
+        fix_data = {'2020-03-12': 590,
+                    '2020-03-15': 1391,
+                    '2020-03-19': 3269}
+
+        self.uk = CovidCountry(country='United Kingdom',
+                               province=True,
+                               filepath='./data/raw/',
+                               fixes=fix_data)
+        self.uk.load()
+
+    def test_smoke(self):
+        """ Test unittest is setup """
+        self.assertEqual(1, 1)
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
