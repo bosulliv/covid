297e35ae7955cba728a30d8266ffde2102acf3d3
Brian Osullivan <brian.osullivan@8x8.com>
2020-04-28 06:58:08 +0100

    increased timeout and manually added singapore for today

--- src/run_all_cron.sh
+++ src/run_all_cron.sh
@@ -4,19 +4,19 @@
 
 compfile=./notebooks/Comparison.ipynb
 skewfilw=./notebooks/Skew\ Function.ipynb
-to=180
+t_o=300
 
 for file in $(ls ./notebooks/*.ipynb)
 do
   if [[ $file != $compfile && $file != $skewfile ]] 
   then
     echo $file
-    ~/anaconda3/bin/jupyter nbconvert --to notebook --execute --ExecutePreprocessor.timeout=$to --inplace $file
+    ~/anaconda3/bin/jupyter nbconvert --to notebook --execute --ExecutePreprocessor.timeout=$t_o --inplace $file
   fi
 done
 
 # Do the comparison after all files
-~/anaconda3/bin/jupyter nbconvert --to notebook --execute --ExecutePreprocessor.timeout=$to --inplace $compfile
+~/anaconda3/bin/jupyter nbconvert --to notebook --execute --ExecutePreprocessor.timeout=$t_o --inplace $compfile
 
 git add .
 git commit -m 'cron update'
